80b27fec7907ed3ea3a66031c147087188bdbc3d
ruzaigh <ruzaigh.kalam2@gmail.com>
2022-04-24 21:02:13 +0200

    busy with nav bar

---
+++ backend_sanity/schemas/testimonials.js
@@ -0,0 +1,30 @@
+export default{
+    name: 'testimonails',
+    title: 'Testimonails',
+    type: 'document',
+    fields: [
+        {
+            name: 'name',
+            title: 'Name',
+            type: 'string'
+        },
+        {
+            name: 'company',
+            title: 'Company',
+            type: 'string'
+        },
+        {
+            name: 'imageurl',
+            title: 'ImageURL',
+            type: 'image',
+            options:{
+                hotspot: true,
+            }
+        },
+        {
+            name: 'feedback',
+            title: 'Feedback',
+            type: 'string'
+        }
+    ]
+}
